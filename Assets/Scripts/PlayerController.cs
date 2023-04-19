1e35c27b46a215f24b0110df186e6e1ac47b2af7
q.thompson <q.thompson088@gmail.com>
2023-04-19 13:50:15 -0700

    Adds bobbing animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public int speed = 10;
+    public GameObject spriteObject;
+    public bool bobbing;
+    public int timer = 10;
+    public string animationState = "growing"; //possible values: "shrinking", "growing"
+    public Vector3 scaleChange = new Vector3(0f, 0.001f, 0f);
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +22,10 @@ public class PlayerController : MonoBehaviour
     void Update() 
     {
         Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Animate();
     }
 
-    void Move(float x, float y)
+    private void Move(float x, float y)
     {
         if (x > 0 && y > 0) {
             //[w] and [d]
@@ -37,8 +44,34 @@ public class PlayerController : MonoBehaviour
             x = -Mathf.Sqrt((x * x) / 2);
             y = Mathf.Sqrt((y * y) / 2);
         }
+        
+        if (x != 0 || y != 0) {
+            bobbing = true;
+        } else {
+            bobbing = false;
+        }
 
         transform.Translate(new Vector2(x, y) * speed * Time.deltaTime);
+    }
+
+    private void Animate() {
+        float maxScale = 1.1f;
+        float minScale = 0.9f;
+        
+        if (animationState == "growing" && bobbing){
+            transform.localScale += scaleChange;
+        } else if (animationState == "shrinking" && bobbing) {
+            transform.localScale -= scaleChange;
+        } else if (!(bobbing)) {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (transform.localScale.y < minScale) {
+            animationState = "growing";
+            scaleChange = new Vector3(0f, 0.001f, 0f);
+        } else if (transform.localScale.y > maxScale) {
+            animationState = "shrinking";
+            scaleChange = new Vector3(0f, 0.002f, 0f);
+        }
 
     }
 }
